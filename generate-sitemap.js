74c36bdec7bd3002ae1b96cd1b03524fb49ce4d0
rimpuhwe <rimpuhwemanzi@gmail.com>
2026-01-01 20:15:45 +0200

    optimising the SEO

---
+++ generate-sitemap.js
@@ -0,0 +1,14 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+
+const sitemap = new SitemapStream({ hostname: 'https://www.abdallahkirombafoundation.com' });
+
+const pages = ['/', '/about', '/programs', '/press', '/founder/biography'];
+
+pages.forEach((page) => {
+  sitemap.write({ url: page, changefreq: 'monthly', priority: 0.8 });
+});
+
+sitemap.end();
+
+streamToPromise(sitemap).then((sm) => createWriteStream('./public/sitemap.xml').write(sm));
