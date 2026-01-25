8e468937efc749bd2ae7b7efd8026e3c94f24ace
rimpuhwe <rimpuhwemanzi@gmail.com>
2026-01-25 13:12:51 +0200

    changed the site domain name

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,7 +1,7 @@
 import { SitemapStream, streamToPromise } from 'sitemap';
 import { createWriteStream } from 'fs';
 
-const sitemap = new SitemapStream({ hostname: 'https://www.abdallahkirombafoundation.com' });
+const sitemap = new SitemapStream({ hostname: 'https://abdallahkirombafoundation.org/' });
 
 const pages = ['/', '/about', '/programs', '/press', '/founder/biography'];
 
